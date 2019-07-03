d176baa6e5140c9462040cce4be6d1ba41771a5f
ALandeau <landeau.alex@gmail.com>
2019-07-03 12:01:14 +0200

    Change engine version

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -42,7 +42,8 @@ const webpackConfig = merge(baseWebpackConfig, {
     }),
     // extract css into its own file
     new MiniCssExtractPlugin({
-      filename: utils.assetsPath('css/[name].[contenthash].css')
+      filename: utils.assetsPath('css/[name].[contenthash].css'),
+      chunkFilename: utils.assetsPath('css/[id].[chunkhash].css')
     }),
     // Compress extracted CSS. We are using this plugin so that possible
     // duplicated CSS from different components can be deduped.
