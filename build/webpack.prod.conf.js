bddd540008e588d907361f0a89f946165f8cd7ef
ALandeau <landeau.alex@gmail.com>
2019-07-03 13:32:26 +0200

    Change wbpack chunks conf

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -24,6 +24,30 @@ const webpackConfig = merge(baseWebpackConfig, {
       extract: true
     })
   },
+  mode: 'production',
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendors: {
+          name: 'vendor',
+          minChunks: function (module, count) {
+            // any required modules inside node_modules are extracted to vendor
+            return (
+              module.resource &&
+              /\.js$/.test(module.resource) &&
+              module.resource.indexOf(
+                path.join(__dirname, '../node_modules')
+              ) === 0
+            )
+          }
+        },
+        manifest: {
+          name: 'manifest',
+          chunks: ['vendor']
+        }
+      }
+    }
+  },
   devtool: config.build.productionSourceMap ? '#source-map' : false,
   output: {
     path: config.build.assetsRoot,
@@ -72,26 +96,6 @@ const webpackConfig = merge(baseWebpackConfig, {
       chunksSortMode: 'dependency',
       serviceWorkerLoader: `<script>${loadMinified(path.join(__dirname,
         './service-worker-prod.js'))}</script>`
-    }),
-    // split vendor js into its own file
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor',
-      minChunks: function (module, count) {
-        // any required modules inside node_modules are extracted to vendor
-        return (
-          module.resource &&
-          /\.js$/.test(module.resource) &&
-          module.resource.indexOf(
-            path.join(__dirname, '../node_modules')
-          ) === 0
-        )
-      }
-    }),
-    // extract webpack runtime and module manifest to its own file in order to
-    // prevent vendor hash from being updated whenever app bundle is updated
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'manifest',
-      chunks: ['vendor']
     }),
     // copy custom static assets
     new CopyWebpackPlugin([
