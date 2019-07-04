bb1253f9157c143ae7138afd495d9fb4821ea7bd
randompiche <antoinepiche@gmail.com>
2019-07-04 16:36:22 +0200

    add: input styling

--- packages/app/src/pages/Home.vue
+++ packages/app/src/pages/Home.vue
@@ -3,7 +3,7 @@
     <h2>Votre nom d'utilisateur</h2>
     <form @submit.prevent="submitUsername">
       <input type="text" v-model="username">
-      <input type="submit" value="submit">
+      <button type="submit">Envoyer</button>
     </form>
   </div>
 </template>
@@ -30,5 +30,21 @@
 </script>
 
 <style scoped>
+input {
+  border-radius: 5px;
+  border: 1px solid whitesmoke;
+  width: 250px;
+  padding: 15px;
+  font-size: 18px;
+}
 
+  button {
+    width: 150px;
+    padding: 15px;
+    border: none;
+    border-radius: 5px;
+    background-color: #3742fa;
+    color: white;
+    font-size: 18px;
+  }
 </style>
